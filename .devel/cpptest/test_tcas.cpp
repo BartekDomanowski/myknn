986ca27e6d0cbaac7bc9cc386e08b1611ce79b9c
Bartlomiej_Domanowski <01187317@pw.edu.pl>
2026-05-26 13:46:30 +0200

    knn brute

--- .devel/cpptest/test_tcas.cpp
+++ .devel/cpptest/test_tcas.cpp
@@ -1,18 +1,23 @@
 #include "../../src/tcas.h"
-#include <algorithm>
 #include <cassert>
 #include <cstdio>
 #include <vector>
 
 int main() {
-    //waw, near waw, fra i guess
     const double lats[] = {52.1657, 52.17, 50.0};
     const double lons[] = {20.9671, 20.97, 8.0};
+
     std::vector<std::size_t> brute;
     std::vector<std::size_t> kd;
     AirRoute::tcas::points_within_radius_km_bruteforce(52.1657, 20.9671, 3, lats, lons, 10.0, brute);
     AirRoute::tcas::points_within_radius_km_kdtree(52.1657, 20.9671, 3, lats, lons, 10.0, kd);
     assert(brute == kd);
+
+    std::vector<std::size_t> knn;
+    AirRoute::tcas::knn_bruteforce(52.1657, 20.9671, 3, lats, lons, 2, knn);
+    assert(knn.size() == 2);
+    assert(knn[0] == 0);
+    assert(knn[1] == 1);
     std::printf("tcas OK\n");
     return 0;
 }
--- src/tcas.h
+++ src/tcas.h
@@ -8,8 +8,8 @@ namespace AirRoute {
 namespace tcas {
 
 void points_within_radius_km_bruteforce(double query_lat, double query_lon, std::size_t n, const double* lats, const double* lons, double radius_km, std::vector<std::size_t>& out_indices);
-
 void points_within_radius_km_kdtree(double query_lat, double query_lon, std::size_t n, const double* lats, const double* lons, double radius_km, std::vector<std::size_t>& out_indices);
+void knn_bruteforce(double query_lat, double query_lon, std::size_t n, const double* lats, const double* lons, std::size_t k, std::vector<std::size_t>& out_indices);
 
 }  // namespace tcas
 }  // namespace AirRoute
